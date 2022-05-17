2d6eeed78a7268538db3bec6877a77dec84ec595
Mehmet <mkaymaz93@gmail.com>
2022-05-17 18:21:22 +0300

    cart, checkout

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css'
 import App from './App'
 import { ProductsProvider } from './context/products_context'
 import { FilterProvider } from './context/filter_context'
-// import { CartProvider } from './context/cart_context'
+import { CartProvider } from './context/cart_context'
 // import { UserProvider } from './context/user_context'
 // import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
     <ProductsProvider>
       <FilterProvider>
-        <App /> 
+        <CartProvider>
+          <App /> 
+        </CartProvider>
       </FilterProvider>
     </ProductsProvider>,
 document.getElementById('root'));
