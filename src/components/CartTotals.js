e12bc31c0e0c5c7478c45c1a70902b4166a14d17
Mehmet <mkaymaz93@gmail.com>
2022-05-17 18:29:35 +0300

    cart

--- src/components/CartTotals.js
+++ src/components/CartTotals.js
@@ -6,25 +6,7 @@ import { formatPrice } from '../utils/helpers'
 import { Link } from 'react-router-dom'
 
 const CartTotals = () => {
-  const { total_amount, shipping_fee } = useCartContext();
-  return (
-    <Wrapper>
-      <div>
-        <article>
-          <h5>subtotal : <span>{formatPrice(total_amount)}</span></h5>
-          <p>shipping fee : <span>{formatPrice(shipping_fee)}</span></p>
-          <hr />
-          <h4>
-            order total :{' '}
-            <span>{formatPrice(total_amount + shipping_fee)}</span>
-          </h4>
-        </article>
-        <Link to="/checkout" className='btn'>
-          proceed to checkout
-        </Link>
-      </div>
-    </Wrapper>
-  )
+  return <h4>cart totals</h4>
 }
 
 const Wrapper = styled.section`
--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ import './index.css'
 import App from './App'
 import { ProductsProvider } from './context/products_context'
 import { FilterProvider } from './context/filter_context'
-import { CartProvider } from './context/cart_context'
+// import { CartProvider } from './context/cart_context'
 // import { UserProvider } from './context/user_context'
 // import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
     <ProductsProvider>
       <FilterProvider>
-        <CartProvider>
-          <App /> 
-        </CartProvider>
+        <App /> 
       </FilterProvider>
     </ProductsProvider>,
 document.getElementById('root'));
