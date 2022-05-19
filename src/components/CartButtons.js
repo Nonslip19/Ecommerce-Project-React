648c2b4581d8638b7e2ea378143c92e92f26c10b
Mehmet <mkaymaz93@gmail.com>
2022-05-19 17:30:51 +0300

    cart

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,18 +8,36 @@ import { useUserContext } from '../context/user_context'
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext()
-  return <Wrapper className='cart-btn-wrapper'>
-    <Link to="/cart" className='cart-btn' onClick={closeSidebar}>
-      Cart
-      <span className='cart-container'>
-        <FaShoppingCart/>
-        <span className='cart-value'>2</span>
-      </span>
-    </Link>
-    <button type='button' className='auth-btn'>
-      Login <FaUserMinus/>
-    </button>
-  </Wrapper>
+  const { total_items, clearCart } = useCartContext()
+  const { loginWithRedirect, myUser, logout } = useUserContext()
+  return (
+    <Wrapper className='cart-btn-wrapper'>
+      <Link to='/cart' className='cart-btn' onClick={closeSidebar}>
+        Cart
+        <span className='cart-container'>
+          <FaShoppingCart />
+          <span className='cart-value'>{total_items}</span>
+        </span>
+      </Link>
+      {myUser ? (
+        <button
+          type='button'
+          className='auth-btn'
+          onClick={() => {
+            clearCart()
+            localStorage.removeItem('user')
+            logout({ returnTo: window.location.origin })
+          }}
+        >
+          Logout <FaUserMinus />
+        </button>
+      ) : (
+        <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+          Login <FaUserPlus />
+        </button>
+      )}
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
@@ -27,14 +45,12 @@ const Wrapper = styled.div`
   grid-template-columns: 1fr 1fr;
   align-items: center;
   width: 225px;
-
   .cart-btn {
     color: var(--clr-grey-1);
     font-size: 1.5rem;
     letter-spacing: var(--spacing);
     color: var(--clr-grey-1);
     display: flex;
-
     align-items: center;
   }
   .cart-container {
--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ import './index.css'
 import App from './App'
 import { ProductsProvider } from './context/products_context'
 import { FilterProvider } from './context/filter_context'
-// import { CartProvider } from './context/cart_context'
-// import { UserProvider } from './context/user_context'
-// import { Auth0Provider } from '@auth0/auth0-react'
-
+import { CartProvider } from './context/cart_context'
+import { UserProvider } from './context/user_context'
+import { Auth0Provider } from '@auth0/auth0-react'
+// dev-pu8wyk-g.us.auth0.com
+// 7vHgXJ01aWGyMVLWdZUwQSUapigdjfKu
 ReactDOM.render(
-    <ProductsProvider>
-      <FilterProvider>
-        <App /> 
-      </FilterProvider>
-    </ProductsProvider>,
-document.getElementById('root'));
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
+
+  document.getElementById('root')
+)
